6bd8c99c9b18368758c234e42a688b3eed41764d
Sarah Tam <sarahtam718@gmail.com>
2019-09-14 12:31:56 -0500

    worked on search bar & submit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar.js';
 import Users from './components/users/Users.js';
 import axios from 'axios';
+import Search from './components/users/Search.js';
 import './App.css';
 
 class App extends Component {
@@ -9,16 +10,23 @@ class App extends Component {
     users: [],
     loading: false
   };
-  async componentDidMount() {
+
+  // Search Github users
+  searchUsers = async text => {
     this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
-    this.setState({ users: res.data, loading: false });
-  }
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Navbar = ({ icon, title }) => {
   return (
-    <div className="navbar bg-primary">
+    <div className='navbar bg-primary'>
       <h1>
         <i className={icon} />
         {title}
@@ -13,8 +13,8 @@ const Navbar = ({ icon, title }) => {
 };
 
 Navbar.defaultProps = {
-  title: "Github Finder",
-  icon: "fab fa-github"
+  title: 'Github Finder',
+  icon: 'fab fa-github'
 };
 
 Navbar.propTypes = {
